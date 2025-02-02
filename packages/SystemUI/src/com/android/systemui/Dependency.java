cc75c5668e7a046eb6f64c9047b230fc51e7acfc
4455bk <rundsxe@gmail.com>
2025-02-03 05:19:01 +0800

    base: Add status bar lyric [1/2]

--- packages/SystemUI/src/com/android/systemui/Dependency.java
+++ packages/SystemUI/src/com/android/systemui/Dependency.java
@@ -136,6 +136,7 @@ public class Dependency {
     @Inject Lazy<NavigationBarController> mNavigationBarController;
     @Inject Lazy<StatusBarStateController> mStatusBarStateController;
     @Inject Lazy<NotificationMediaManager> mNotificationMediaManager;
+    @Inject Lazy<NotificationListener> mNotificationListener;
     @Inject @Background Lazy<Looper> mBgLooper;
     @Inject @Named(TIME_TICK_HANDLER_NAME) Lazy<Handler> mTimeTickHandler;
     @Inject Lazy<SysUiState> mSysUiStateFlagsContainer;
@@ -183,6 +184,7 @@ public class Dependency {
         mProviders.put(NavigationBarController.class, mNavigationBarController::get);
         mProviders.put(StatusBarStateController.class, mStatusBarStateController::get);
         mProviders.put(NotificationMediaManager.class, mNotificationMediaManager::get);
+        mProviders.put(NotificationListener.class, mNotificationListener::get);
         mProviders.put(SysUiState.class, mSysUiStateFlagsContainer::get);
         mProviders.put(CommandQueue.class, mCommandQueue::get);
         mProviders.put(UiEventLogger.class, mUiEventLogger::get);
